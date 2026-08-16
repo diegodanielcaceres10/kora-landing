4c48bc278286796481c3288c17b0fa77ad2150be
Diego Daniel Caceres <diegodanielcaceres10@gmail.com>
2026-08-16 14:07:31 +0200

    add: GitHub Pages deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: "/",
+  base: command === "build" ? "/kora-landing/" : "/",
   server: {
     watch: {
       usePolling: true,
     },
     host: true,
   },
-});
+}));
