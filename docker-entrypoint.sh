f6aab54fa22d5c19483b0ea31d1111acf589c59a
Diego Daniel Caceres <diegodanielcaceres10@gmail.com>
2026-08-19 18:57:46 +0200

    Fix local runner

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Exit immediately if a command exits with a non-zero status
 set -e
-echo "Starting Kora Langing Page frontend..."
+echo "Starting Kora Roster Page frontend..."
 exec npm run dev -- --host
